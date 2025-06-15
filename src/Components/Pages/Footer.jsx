da138d4cd3ac80870f4e4a3b2b3fdfef740f41b7
Gifty Arthur <giftyarthur031@gmail.com>
2025-06-15 14:47:30 +0000

    home

--- src/Components/Pages/Footer.jsx
+++ src/Components/Pages/Footer.jsx
@@ -1,7 +1,68 @@
 import React from "react";
 
+import dhu from "../../assets/dhu.png";
+
+import hu from "../../assets/hu.png";
+import shu from "../../assets/shu.png";
+
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <div>
+      <div className="mt-30">
+        {/* Tablet Image */}
+        <img
+          src={hu}
+          alt="tablet"
+          className="w-full hidden md:block lg:hidden"
+        />
+
+        {/* Mobile Image */}
+        <img src={shu} alt="mobile" className="w-full block md:hidden" />
+
+        {/* Mobile & Tablet Heading */}
+        <h1 className="text-[32px] md:text-[40px] font-bold text-center font-Manrope text-custom5 block lg:hidden mt-6">
+          BRINGING YOU THE <span className="text-primary">BEST</span> AUDIO GEAR
+        </h1>
+
+        {/* Mobile & Tablet Paragraph */}
+        <p className="text-[15px]  font-medium font-Manrope text-[#919191] text-center mt-4 px-4 block lg:hidden">
+          Located at the heart of New York City, Audiophile is the premier store
+          for high end headphones, earphones, speakers, and audio accessories.
+          We have a large showroom and luxury demonstration rooms available for
+          you to browse and experience a wide range of our products. Stop by our
+          store to meet some of the fantastic people who make Audiophile the
+          best place to buy your portable audio equipment.
+        </p>
+
+        {/* Web (Desktop) Layout */}
+        <div className="hidden lg:flex items-center justify-between px-10">
+          <div>
+            <h1 className="text-[40px] font-bold font-Manrope text-custom5">
+              BRINGING YOU THE
+              <br /> <span className="text-primary">BEST</span> AUDIO GEAR
+            </h1>
+            <p className="text-[15px] font-medium font-Manrope text-[#919191] mt-4">
+              Located at the heart of New York City, Audiophile is the premier
+              <br />
+              store for high end headphones, earphones, speakers, and audio
+              <br />
+              accessories. We have a large showroom and luxury <br />
+              demonstration rooms available for you to browse and
+              <br /> experience a wide range of our products. Stop by our store
+              to <br />
+              meet some of the fantastic people who make Audiophile the
+              <br /> best place to buy your portable audio equipment.
+            </p>
+          </div>
+          <img
+            src={dhu}
+            alt="desktop"
+            className="hidden lg:block w-[540px] h-[588px]"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/Components/SubHome.jsx
+++ src/Components/SubHome.jsx
@@ -5,29 +5,34 @@ import imgs from "../assets/assets/home/desktop/imgs.jpg";
 import sm from "../assets/assets/home/mobile/sm.jpg";
 import speaker from "../assets/assets/home/desktop/speaker.png";
 import tab from "../assets/assets/home/tablet/tabSpeaker.jpg";
+
 import SeeProductButton from "./SeeProduct";
+import ear from "../assets/assets/home/desktop/ear.jpg";
 
 const SubHome = () => {
   return (
     <div>
       <div className="relative w-full">
         {/* Background image */}
-        <img src={bg} alt="Background" className="w-full hidden md:block " />
+        <img src={bg} alt="Background" className="w-full hidden lg:block " />
         <img
           src={mbg}
           alt="Background"
-          className="w-full h-[600px] block  md:hidden "
+          className="w-full h-[600px] block lg:hidden"
         />
 
-        <div className="absolute inset-0 flex flex-col items-center px-4">
-          <div className="flex flex-col md:flex-row md:items-center md:justify-between md:gap-50 px-4 md:px-20">
+        <div className="absolute inset-0 flex flex-col lg:flex-row items-center lg:items-end px-4">
+          {/* For mobile and tablet: stack vertically. For desktop (lg+): side by side */}
+          <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between lg:gap-50 px-4 lg:px-20">
+            {/* Speaker Image */}
             <img
               src={speaker}
               alt="ZX9 Speaker"
-              className="md:w-[410px] md:h-[493px] w-[172px] h-[207px] md:mt-38 mt-20 mx-auto md:mx-0"
+              className="lg:w-[410px] lg:h-[493px] w-[172px] h-[207px] mt-20 mx-auto lg:mx-0"
             />
 
-            <div className="mt-6 md:mt-10 flex flex-col items-center text-center md:items-start md:text-left">
+            {/* Text Section */}
+            <div className="mt-6 lg:mt-10 flex flex-col items-center text-center lg:items-start lg:text-left">
               <h1 className="text-white text-4xl lg:text-[56px] font-bold font-Manrope">
                 ZX9
                 <br />
@@ -35,11 +40,12 @@ const SubHome = () => {
               </h1>
 
               <p className="mt-4 text-[#f5dfd2] text-[15px] font-medium font-Manrope">
-                <span className="block md:hidden">
-                  Upgrade to premium speakers that are phenomenally built to
-                  deliver truly remarkable sound.
+                <span className="block lg:hidden text-center">
+                  Upgrade to premium speakers that are
+                  <br /> phenomenally built to deliver truly remarkable <br />
+                  sound.
                 </span>
-                <span className="hidden md:block">
+                <span className="hidden lg:block">
                   Upgrade to premium speakers that are <br />
                   phenomenally built to deliver truly remarkable <br />
                   sound.
@@ -84,6 +90,45 @@ const SubHome = () => {
           </div>
         </div>
       </div>
+      {/* Earphone */}
+
+      <div className="px-4 mt-20">
+        <div className="w-full px-4 md:px-8 lg:px-0 mt-20">
+          {/* Desktop & Tablet View */}
+          <div className="hidden md:flex items-center justify-center md:gap-6 lg:space-x-18">
+            <img
+              src={ear}
+              alt=""
+              className="rounded-lg w-full md:w-1/2  h-[320px]"
+            />
+            <div className="w-full md:w-1/2 lg:w-[540px] h-[320px] md:[540px] bg-[#f1f1f1f1] rounded-lg flex items-center justify-center">
+              <div className="">
+                <h1 className="text-[28px] font-bold font-Manrope text-custom5">
+                  YX1 EARPHONES
+                </h1>
+                <SeeProductButton className="bg-custom5 hover:bg-[#4c4c4c] text-white mt-4" />
+              </div>
+            </div>
+          </div>
+
+          {/* Mobile View */}
+          <div className="block md:hidden space-y-4">
+            <img src={ear} alt="" className="rounded-lg w-full" />
+            <div className="w-full bg-[#f1f1f1f1] rounded-lg py-10 flex justify-center">
+              <div>
+                <h1 className="text-[28px] font-bold font-Manrope text-custom5 text-center">
+                  YX1 EARPHONES
+                </h1>
+                <div className="flex justify-center">
+                  <SeeProductButton className="bg-transparent border-2 border-black text-black hover:text-white hover:bg-custom5 mt-4" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* AUDIO  */}
     </div>
   );
 };
